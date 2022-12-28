13d93b6598af7ae0e8d518451b6d4b233eec5083
Rizaldy Christoffel <31089448+fireorb@users.noreply.github.com>
2022-12-29 05:07:00 +0700

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -16,3 +16,20 @@ sudo apt install jenkins -y
 sudo service jenkins start
 sudo systemctl enable jenkins
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+
+###########
+sudo apt update
+sudo apt install default-jdk
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+
+
+
